230ff251913cfee3bb18fa23a514c49ff33a3783
Liam Snow <liamsnow03@gmail.com>
2024-03-21 12:01:13 -0400

    Initial Snake Version Complete

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Sigma-Pi-Stair-Lights
- I am not in Sigma Pi
+Arduino code an ESP32 that controls two addressable LED strips (1200 LEDs total)
+to pulse down the stairs in a variety of patterns.
+
+## Running
+1. Install Arduino IDE
+2. Open this dir arduino/arduino.ino
+3. Navigate to File > Preferences
+4. Set "Additional boards manager URLs" to https://dl.espressif.com/dl/package_esp32_index.json
+5. Open Boards Manager in left nav
+6. Search for "esp32 by Espressif" and install
+7. Open Library Manager in left nav
+8. Search for "FastLED by Daniel Garcia" and install
+9. Plug in ESP32
+10. Click on Devices in the top nav (next to update & debug buttons)
+11. Edit the plugged in Device (Not COMX)
+12. Select "DOIT ESP32 DEVKIT Board"
---
+++ arduino/arduino.ino
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <FastLED.h>
+
+#define NUM_LEDS 300
+#define DATA_PIN 39
+#define SNAKE_LENGTH 10
+#define NUM_SNAKES 7
+
+CRGB leds[NUM_LEDS];
+int snakePos[NUM_SNAKES];
+
+void setup() {
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  for (int i = 0; i < NUM_SNAKES; i++) {
+    snakePos[i] = (NUM_LEDS / NUM_SNAKES) * i;
+  }
+}
+
+void loop() {
+  for (int i = 0; i < NUM_SNAKES; i++) {
+    snakePos[i] = (snakePos[i] + 1) % NUM_LEDS;
+  }
+  displaySnakes();
+  delay(1);
+}
+
+void displaySnakes() {
+    fill_solid(leds, NUM_LEDS, CRGB::Black);
+    
+    for (int j = 0; j < NUM_SNAKES; j++) {
+        //draw each snake
+        for (int i = 0; i < SNAKE_LENGTH; i++) {
+            int pos = (snakePos[j] + i) % NUM_LEDS;
+            // leds[pos] = CRGB::Green;
+            leds[pos] = CRGB(100, round(((float)i / SNAKE_LENGTH) * 255), 50);
+            // leds[pos] = CHSV(round(((float)i / SNAKE_LENGTH) * 255), 255, 255);
+        }
+    }
+    
+    FastLED.show();
+}
